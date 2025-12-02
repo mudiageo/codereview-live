89c0ab126b578127a5629d03108ecf1a0bd8e253
Mudia Geo <mudiageo@gmail.com>
2025-12-02 17:05:51 +0000

    init shadcn-svelte with mode-watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+ import { ModeWatcher } from "mode-watcher";
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	
 	let { children } = $props();
 </script>
-
+ 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<ModeWatcher />
 {@render children()}
