da68c28caec31327b2b771ce7d584572b127dddf
Mudia Geo <mudiageo@gmail.com>
2025-12-09 17:20:01 +0000

    fix errors

--- src/lib/utils/p2p-sharing.ts
+++ src/lib/utils/p2p-sharing.ts
@@ -1,4 +1,4 @@
-import Peer, { DataConnection } from 'peerjs';
+import Peer, { type DataConnection } from 'peerjs';
 import { EncryptionUtil } from './encryption';
 
 export interface P2PCallbacks {
