d87ad827e99ecb8f9047910a8f34bff83485b592
Mudia Geo <mudiageo@gmail.com>
2026-01-01 14:43:40 +0000

    change redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   
   onMount(() => {
     // Redirect to your default route
-    goto('/dashboard');
+    goto('/login');
   });
 </script><h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
